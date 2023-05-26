a2506629d1434c108a45a8f4d56e24a7e013a77f
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2023-05-26 08:16:00 -0700

    13

--- simple_shell.c
+++ simple_shell.c
@@ -7,53 +7,58 @@
 #define MAX_COMMAND_LENGTH 100
 
 /**
- * Displays the prompt symbol to indicate the shell is ready for user input.
+ * display_prompt - Displays the prompt symbol to indicate the shell is ready for user input.
  */
 void display_prompt(void)
 {
-    printf("> ");
-    fflush(stdout);
+	printf("> ");
+	fflush(stdout);
 }
 
+/**
+ * main - Entry point for the shell program.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    char command[MAX_COMMAND_LENGTH];
+	char command[MAX_COMMAND_LENGTH];
 
-    while (1)
-    {
-        display_prompt();
+	while (1)
+	{
+		display_prompt();
 
-        if (fgets(command, sizeof(command), stdin) == NULL)
-        {
-            printf("\n");
-            break;  /* End of file (Ctrl+D) condition */
-        }
+		if (fgets(command, sizeof(command), stdin) == NULL)
+		{
+			printf("\n");
+			break;  /* End of file (Ctrl+D) condition */
+		}
 
-        command[strcspn(command, "\n")] = '\0';  /* Remove the trailing newline character */
+		command[strcspn(command, "\n")] = '\0';  /* Remove the trailing newline character */
 
-        pid_t pid = fork();
+		pid_t pid = fork();
 
-        if (pid == -1)
-        {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-        {
-            /* Child process */
-            if (execlp(command, command, NULL) == -1)
-            {
-                perror("execlp");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            /* Parent process */
-            int status;
-            waitpid(pid, &status, 0);
-        }
-    }
+		if (pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			/* Child process */
+			if (execlp(command, command, NULL) == -1)
+			{
+				perror("execlp");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			/* Parent process */
+			int status;
+			waitpid(pid, &status, 0);
+		}
+	}
 
-    return 0;
+	return 0;
 }
