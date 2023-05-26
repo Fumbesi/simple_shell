5b9ffe96b1f887bdbe1654f0fae30e18e3fa909c
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2023-05-26 11:31:15 -0700

    26

--- _atoi.c
+++ _atoi.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * interactive - checks if the shell is in interactive mode
- * @info: pointer to the info_t structure
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
  *
- * Return: 1 if the shell is in interactive mode, 0 otherwise
+ * Return: 1 if interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
 {
@@ -12,32 +12,28 @@ int interactive(info_t *info)
 }
 
 /**
- * is_delim - checks if a character is a delimiter
- * @c: character to check
- * @delim: delimiter string
- *
- * Return: 1 if the character is a delimiter, 0 otherwise
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *delim)
 {
 	while (*delim)
-	{
-		if (*delim == c)
+		if (*delim++ == c)
 			return (1);
-		delim++;
-	}
 	return (0);
 }
 
 /**
- * _isalpha - checks for an alphabetic character
- * @c: character to check
- *
- * Return: 1 if the character is alphabetic, 0 otherwise
+ * _isalpha - checks for alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
  */
+
 int _isalpha(int c)
 {
-	if ((c >= 'b' && c <= 'x') || (c >= 'B' && c <= 'X'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
@@ -45,16 +41,16 @@ int _isalpha(int c)
 
 /**
  * _atoi - converts a string to an integer
- * @s: string to convert
- *
- * Return: the converted integer, or 0 if no numbers in the string
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
  */
+
 int _atoi(char *s)
 {
-	int sign = 1, flag = 0, output = 0;
+	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (int i = 0; s[i] != '\0' && flag != 2; i++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
 		if (s[i] == '-')
 			sign *= -1;
